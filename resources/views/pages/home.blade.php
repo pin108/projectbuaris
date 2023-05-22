22cea1a59df3115a403e7eae1c36c81b9843ecca
kukuh-biyantama <kukuhbiyantama2244@gmail.com>
2023-05-23 00:55:23 +0700

    update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -50,7 +50,7 @@
 	<!-- /.container -->
 </div>
 <!-- = /. Header Section = -->
-<!-- <div class=" container py-60 py-lg-100">
+<div class=" container py-60 py-lg-100">
 	<div class="swiper image-slider client-logo-slider mx-xl-34 mx-xxl-80">
 		<div class="swiper-wrapper">
 			<div class="swiper-slide">
@@ -73,7 +73,7 @@
 			</div>
 		</div>
 	</div>
-</div> -->
+</div>
 @stop
 
 @section('content')
