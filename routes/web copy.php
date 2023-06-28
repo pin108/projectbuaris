17957bed0e4b1f172de3c5732dad6c48fd116707
Alvin Steven <89818753+pin108@users.noreply.github.com>
2023-06-29 00:42:22 +0700

    update

--- routes/web copy.php
+++ routes/web copy.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,29 +17,19 @@ Route::get('/', function () {
     return view('pages.home');
 });
 
-Route::get('/dashboard', function () {
-    return view('pages.home');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/prof     ile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//galang dana
+Route::get('/galangdana', function () {
+    return view('pages.galangdana.galangdana');
 });
 
-<<<<<<< Updated upstream
 //galang dana lainnya
 Route::get('/galangdanalainnya', function () {
     return view('pages.galangdana.galangdanalainnya');
-})->name('galangdanalainnya');
-
-Route::get('/registergalangdana', function () {
-    return view('pages.galangdana.buatgalangdana');
-})->name('registergalangdana');
+});
 
 Route::get('/daftar', function () {
     return view('pages.signup');
-})->name('daftar');
+});
 
 Route::get('/login', function () {
     return view('pages.login');
@@ -56,8 +45,7 @@ Route::get('/showpengalangdana', function () {
 
 Route::get('/kirimdonasi', function () {
     return view('pages.detaildonasi');
-})->name('kirimdonasi');
-
+});
 Route::get('/adminpengalang', function () {
     return view('pages.adminpengalang');
 });
@@ -70,6 +58,3 @@ Route::get('/informasibeasiswa', function () {
 Route::get('/registerbeasiswa', function () {
     return view('pages.beasiswa.pendaftaran');
 });
-=======
-require __DIR__.'/auth.php';
->>>>>>> Stashed changes
