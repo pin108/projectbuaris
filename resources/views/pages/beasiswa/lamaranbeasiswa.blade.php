6b8308832cbc2dc6a20bddc45112cb2d1cc82084
kukuh-biyantama <kukuhbiyantama2244@gmail.com>
2023-08-21 11:39:41 +0700

    update beasiswa

--- resources/views/pages/beasiswa/lamaranbeasiswa.blade.php
+++ resources/views/pages/beasiswa/lamaranbeasiswa.blade.php
@@ -6,15 +6,10 @@
         @foreach ($pendaftarans as $pendaftaran)
         <div class="col-md-4 mb-4">
             <div class="card">
+                <img src="https://www.indonesiacollege.co.id/wp-content/uploads/2023/04/beasiswa.jpg" class="card-img-top" alt="Donation Image">
                 <div class="card-body">
-                    <div class="row">
-                        <div>
-                            <p class="card-text">Jenis: {{ $pendaftaran->kategoribeasiswa->jenis_kategoribeasiswa }}</p>
-                        </div>
-                        <div>
-                            <p class="card-text">{{ $pendaftaran->kategoribeasiswa->judul_kategoribeasiswa }}</p>
-                        </div>
-                    </div>
+                    <h5 class="card-title">Jenis: {{ $pendaftaran->kategoribeasiswa->jenis_kategoribeasiswa }}</h5>
+                    <p class="card-text">{{ $pendaftaran->kategoribeasiswa->judul_kategoribeasiswa }}</p>
                     @if ($pendaftaran->is_active == 1)
                     <span class="badge badge-success" style="background-color: green;">Lulus</span>
                     @elseif ($pendaftaran->is_active == 2)
